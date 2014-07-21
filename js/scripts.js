2bd02eface51b0d89a7bf4c6c4119d1c06ead239
Blake Mason & Diane Douglas <student@epicodus.com>
2014-07-21 15:10:57 -0700

    Add refactoring of prototype to relegate all jquerying to the inside of the (document).ready--particularly the displayStatus function--.

--- js/scripts.js
+++ js/scripts.js
@@ -4,89 +4,89 @@ var Tamagotchi = {
     this.foodLevel = 10;
     this.sleepLevel = 10;
     this.activityLevel = 10;
-    displayStatus();
   },
   timePasses: function(){
     this.foodLevel -= 1;
     this.sleepLevel -= 1;
     this.activityLevel -= 1;
-    displayStatus();
   },
   isAlive: function(){
     if ((this.sleepLevel <= 0) && (this.activityLevel <= 0)) {
-      $(".dead").fadeIn();
-      $(".interactions, .status, .info").hide();
-      clearInterval(interval);
       return false;
     } else if (this.foodLevel > 0){
       return true;
     } else {
-      $(".dead").fadeIn();
-      $(".interactions, .status, .info").hide();
-      clearInterval(interval);
       return false;
     }
   },
   feeding: function(){
     this.foodLevel += 1;
-    displayStatus();
   },
   playing: function() {
     this.activityLevel += 1;
-    displayStatus();
   },
   sleeping: function(){
     this.sleepLevel += 1;
-    displayStatus();
   },
 };
 
-var gotchi;
-var interval;
-var displayStatus = function(){
-  var foodStatus = $(".food");
-  var sleepStatus = $(".sleep");
-  var activityStatus = $(".activity");
-  foodStatus.text(gotchi.foodLevel);
-  sleepStatus.text(gotchi.sleepLevel);
-  activityStatus.text(gotchi.activityLevel);
-  if(gotchi.foodLevel <= 3){
-    foodStatus.addClass('emergency-text');
-  }else{
-    foodStatus.removeClass('emergency-text');
-  }
-  if(gotchi.sleepLevel <= 3){
-    sleepStatus.addClass('emergency-text');
-  }else{
-    sleepStatus.removeClass('emergency-text');
-  }
-  if(gotchi.activityLevel <= 3){
-    activityStatus.addClass('emergency-text');
-  }else{
-    activityStatus.removeClass('emergency-text');
-  }
-  gotchi.isAlive();
-}
 
 $(document).ready(function(){
+  var gotchi;
+  var interval;
+  var displayStatus = function(){
+    var foodStatus = $(".food");
+    var sleepStatus = $(".sleep");
+    var activityStatus = $(".activity");
+    foodStatus.text(gotchi.foodLevel);
+    sleepStatus.text(gotchi.sleepLevel);
+    activityStatus.text(gotchi.activityLevel);
+    if(gotchi.foodLevel <= 3){
+      foodStatus.addClass('emergency-text');
+    }else{
+      foodStatus.removeClass('emergency-text');
+    }
+    if(gotchi.sleepLevel <= 3){
+      sleepStatus.addClass('emergency-text');
+    }else{
+      sleepStatus.removeClass('emergency-text');
+    }
+    if(gotchi.activityLevel <= 3){
+      activityStatus.addClass('emergency-text');
+    }else{
+      activityStatus.removeClass('emergency-text');
+    }
+    if(!gotchi.isAlive()){
+      $(".dead").fadeIn();
+      $(".interactions, .status, .info").hide();
+      clearInterval(interval);
+    }
+  }
   $("form").submit(function( event ) {
     event.preventDefault();
     gotchi = Object.create(Tamagotchi);
     gotchi.initialize($("input#pet-name").val());
+    displayStatus();
     $(".your-pet").text($("input#pet-name").val() + "!");
     $(".interactions").slideDown();
     $(".status").slideDown();
     $("form").hide();
-    interval = setInterval(function(){ gotchi.timePasses()}, 1000);
+    interval = setInterval(function(){
+      gotchi.timePasses();
+      displayStatus();
+    }, 1000);
   });
   $("#feeding").click(function(){
     gotchi.feeding();
+    displayStatus();
   });
   $("#playing").click(function(){
     gotchi.playing();
+    displayStatus();
   });
   $("#sleeping").click(function(){
     gotchi.sleeping();
+    displayStatus();
   });
   $(".restart-button").click(function(){
     $(".dead").hide();
